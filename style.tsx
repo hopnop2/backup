1e8a5d6d672b95950925287938bebdf68a957386
hopnop2 <hopnop080@gmail.com>
2025-03-05 21:34:03 +0700

    version 1

--- style.tsx
+++ style.tsx
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 
 const MyStyle = StyleSheet.create({
   textTest: {
-    color: '#ff6f00',
+    color: '#fff',
     fontSize: 20,
     fontWeight: 'bold',
   },
